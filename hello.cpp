d57637f72b02a068d8509254df4fae44c40c7fc5
昵称 <procedureMaker@163.com>
2026-01-21 21:22:19 +0800

    hello

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    if (a > 0)
+        cout << "hello" << endl;
+    else
+        cout << -1 << endl;
+    return 0;
+}
