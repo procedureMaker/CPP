d90b7a0eabbd14c33fbc4d8886639309df18e8c3
昵称 <procedureMaker@163.com>
2026-02-26 23:05:20 +0800

    重新克隆重新上传

--- 20260121/hello.cpp
+++ 20260121/hello.cpp
@@ -1,14 +1,56 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
+mt19937 rng((unsigned)chrono::high_resolution_clock::now().time_since_epoch().count());
+
+int partition(vector<int> &a, int l, int r)
+{
+    int pivot = a[r];
+    int i = l - 1;
+    for (int j = l; j < r; ++j)
+    {
+        if (a[j] <= pivot)
+        {
+            ++i;
+            swap(a[i], a[j]);
+        }
+    }
+    swap(a[i + 1], a[r]);
+    return i + 1;
+}
+
+void quicksort(vector<int> &a, int l, int r)
+{
+    if (l >= r)
+        return;
+    // 随机选择枢轴并放到右端
+    uniform_int_distribution<int> dist(l, r);
+    swap(a[dist(rng)], a[r]);
+    int p = partition(a, l, r);
+    quicksort(a, l, p - 1);
+    quicksort(a, p + 1, r);
+}
 
 int main()
 {
-    int a;
-    cin >> a;
-    if (a > 0)
-        cout << "hello" << endl;
-    else
-        cout << -1 << endl;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    if (!(cin >> n))
+        return 0;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i)
+        cin >> a[i];
+
+    quicksort(a, 0, n - 1);
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (i)
+            cout << ' ';
+        cout << a[i];
+    }
+    cout << '\n';
     return 0;
 }
---
+++ 20260226/test.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    float a = 1.5;
+    cout << (int)a << endl;
+
+    float b = 1.4;
+    cout << (int)b << endl;
+
+    float c = -1.5;
+    cout << (int)c << endl;
+
+    float d = -1.4;
+    cout << (int)d << endl;
+
+    float e = 1.6;
+    cout << (int)e << endl;
+
+    float f = -1.6;
+    cout << (int)f << endl;
+
+    return 0;
+}
